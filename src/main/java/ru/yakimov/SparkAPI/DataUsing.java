c5de274783c44cbc1c3ed317f92a109e6128b294
Vladimir Iakimov <yakimovvn@bk.ru>
2019-08-25 15:09:14 +0300

    Have created Class DataUsing and methods ti unloading users to JSon files

---
+++ src/main/java/ru/yakimov/SparkAPI/DataUsing.java
@@ -0,0 +1,46 @@
+package ru.yakimov.SparkAPI;
+
+import net.arnx.jsonic.JSON;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DataUsing {
+
+
+    private static <T>void convertToJSonic(T obj, String filePath) throws IOException {
+        try(FileWriter fileWriter = new FileWriter(filePath)){
+            fileWriter.write(JSON.encode(obj));
+        }
+    }
+
+    private static void unloadingNewUsersWithDate (int howMany, String toDir){
+        File dir = new File(toDir);
+        boolean isThereDir = true;
+
+        if(!dir.exists())
+            isThereDir = dir.mkdirs();
+
+        if(!isThereDir)
+            return;
+
+        for (int i = 0; i < howMany; i++) {
+            UserWithData user = new UserWithData(1+i,
+                    "Владимир"+(i+1),
+                    "+7"+ (int) (Math.random() * 1000000000),
+                    "high");
+            try {
+                convertToJSonic(user, dir.getPath()+"/user"+(i+1)+".json");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        unloadingNewUsersWithDate(150,"user");
+    }
+
+
+}
