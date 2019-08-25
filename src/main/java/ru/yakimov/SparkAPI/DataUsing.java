de0e99fcaa0bdf1f4a17e676f525cf2941e2405e
Vladimir Iakimov <yakimovvn@bk.ru>
2019-08-26 00:12:12 +0300

    Write class save to Avro.

--- src/main/java/ru/yakimov/SparkAPI/DataUsing.java
+++ src/main/java/ru/yakimov/SparkAPI/DataUsing.java
@@ -1,32 +1,27 @@
 package ru.yakimov.SparkAPI;
 
 import net.arnx.jsonic.JSON;
-import net.arnx.jsonic.JSONReader;
-import net.arnx.jsonic.JSONWriter;
 import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.*;
-import org.apache.derby.iapi.services.io.ArrayInputStream;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.DataFrameWriter;
-import org.apache.spark.sql.execution.columnar.compression.RunLengthEncoding;
-import org.mortbay.util.ajax.JSONObjectConvertor;
-import scala.util.parsing.json.JSONObject;
 
 import java.io.*;
+import java.util.*;
 
 public class DataUsing {
-    public static final String DIR_JSON = "user";
-    public static final String PATH_SCHEMA = "src/main/resources/UserWhithData.avsc";
-    public static final String OUTPUT_FILE_PATH = "src/main/resources/User.avro"
-    public static final JavaSparkContext SC = new JavaSparkContext(
+    private static final String DIR_JSON = "user";
+    private static final String OUTPUT_FILE_PATH = "src/main/resources/User.avro";
+    private static final String PATH_SCHEMA = "src/main/resources/UserWhithData.avsc";
+    private static final JavaSparkContext SC = new JavaSparkContext(
             new SparkConf().setMaster("local").setAppName("MyApp"));
+    private static final Schema SCHEMA = new Schema.Parser()
+            .parse( SC.textFile(PATH_SCHEMA).first() );
 
 
     private static <T>void convertToJSonic(T obj, String filePath) throws IOException {
@@ -58,85 +53,86 @@ public class DataUsing {
         }
     }
 
-    private static void writeSchemaToAvro(Schema schema, String path){
+    private static void writeSchemaAvro(String path){
         try(FileWriter fileWriter = new FileWriter(path)){
-            fileWriter.write(schema.toString());
+            fileWriter.write(SCHEMA.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static JavaRDD<String> getRDDJsonsString(String dir){
-        return SC.textFile(dir+"/*.json");
+    private static JavaRDD<GenericData.Record> getRDDJsonsString(String dir){
+        return SC.textFile(dir+"/*.json")
+                .map(DataUsing::dataJson)
+                .map(DataUsing ::getRecord);
+    }
 
+    private static String[] dataJson(String json){
+        char[] charJson = json.toCharArray();
+        for (int i = 0; i < charJson.length; i++) {
+            switch (charJson[i]){
+                case '{':
+                case '}':
+                case '"':
+                case ':':
+                case ',':
+                    charJson[i] = ' ';
+
+            }
+        }
+        return removeSpaces(new String(charJson).split(" "));
+    }
+
+    private static String [] removeSpaces(String[] strArr){
+        ArrayList<String> arrayList = new ArrayList<>();
+        int count = 1;
+        for (String str : strArr) {
+            if(!str.equals("") && count++%2 ==0)
+                arrayList.add(str);
+        }
+        return  arrayList.toArray(new String[0]);
     }
 
 
-    private static Schema getSchemaString(String pathAvtoSchema){
-        return new Schema.Parser()
-                .parse( SC.textFile(pathAvtoSchema).first() );
-    }
 
-    private static DataFileWriter getFileWrite(Schema schema) throws IOException {
+    private static DataFileWriter<GenericRecord> getFileWrite() throws IOException {
 
-//        Schema schema = getSchemaString(PATH_SCHEMA);
-        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(schema);
+        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(SCHEMA);
         DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter);
-        dataFileWriter.create(schema, new File(OUTPUT_FILE_PATH));
+        dataFileWriter.create(SCHEMA, new File(OUTPUT_FILE_PATH));
         return dataFileWriter;
     }
 
-    public static GenericData.Record getRecourd(Schema schema, String json){
-
-        GenericData.Record record = new GenericData.Record(schema);
-        record.put("user_id", Long.parseLong(json))
+    public static GenericData.Record getRecord(String [] jsonDataArr){
+        GenericData.Record record = new GenericData.Record(SCHEMA);
+        record.put("user_id", Long.parseLong(jsonDataArr[0]));
+        record.put("user_name", jsonDataArr[1]);
+        record.put("user_phone", Long.parseLong(jsonDataArr[2]));
         return record;
     }
 
+    private static void saveToAvro(){
+        try (DataFileWriter<GenericRecord> dataFileWriter = getFileWrite()){
+            getRDDJsonsString(DIR_JSON).foreach(dataFileWriter::append);
 
-
-//    private static
-
-
-
-//    private byte[] fromJsonToAvroByteCode(String json, String schemaStr) throws IOException {
-//        InputStream input = new ArrayInputStream(json.getBytes());
-//        DataInputStream dataInputStream = new DataInputStream(input);
-//        Schema schema = Schema.parse(schemaStr);
-//        Decoder decoder = DecoderFactory.get().jsonDecoder(schema, dataInputStream);
-//        DatumReader<Object> reader = new GenericDatumReader<>(schema);
-//        Object datum = reader.read(null, decoder);
-//
-//        GenericDatumWriter<Object> writer = new GenericDatumWriter<>(schema);
-//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//
-//        Encoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-//        writer.write(datum, encoder);
-//        encoder.flush();
-//        return outputStream.toByteArray();
-//    }
-
-
-
-
-
-
-
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public static void main(String[] args) {
+
+
         final int countJson = 150;
+
 
         unloadingNewUsersWithDate(countJson,DIR_JSON);
 
-        writeSchemaToAvro(UserWithData.createAvroSchema(),PATH_SCHEMA);
+        writeSchemaAvro(PATH_SCHEMA);
 
-//        System.out.println(getRDDSchemaString(PATH_SCHEMA));
+        saveToAvro();
 
-//        for (String arg : getRDDJsonsString(DIR_JSON).collect()) {
-//            System.out.println(arg);
-//        }
 
 
 
